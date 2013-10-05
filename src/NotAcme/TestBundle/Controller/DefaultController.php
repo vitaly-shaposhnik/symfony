638ce7e9aa30bb775ae7ce05c7d27236432bae1a
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-10-06 00:01:28 +0300

    Add bundle

---
+++ src/NotAcme/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NotAcme\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('NotAcmeTestBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/NotAcme/TestBundle/NotAcmeTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NotAcme\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NotAcmeTestBundle extends Bundle
+{
+}
