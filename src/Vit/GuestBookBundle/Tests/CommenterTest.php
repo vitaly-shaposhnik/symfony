831bc482ae28fb099a0160ccc1d1c2779491cec3
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-10-29 23:26:23 +0200

    Add tests

--- src/Vit/GuestBookBundle/Tests/CommenterTest.php
+++ src/Vit/GuestBookBundle/Tests/CommenterTest.php
@@ -12,21 +12,46 @@ namespace Vit\GuestBookBundle\Tests;
 
 class CommenterTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @dataProvider provider
-     */
-    public function testAdd($a, $b, $c)
-    {
-        $this->assertEquals($c, $a + $b);
-    }
+//    public function provider()
+//    {
+//        return array(
+//            array(0, 0, 0),
+//            array(0, 1, 1),
+//            array(1, 0, 1),
+//        );
+//    }
 
-    public function provider()
+    /**
+     * @ dataProvider provider
+     */
+//    public function testAdd($a, $b, $c)
+//    {
+//        $this->assertEquals($c, $a + $b);
+//    }
+
+    public function calcData()
     {
         return array(
-            array(0, 0, 0),
-            array(0, 1, 1),
-            array(1, 0, 1),
-            array(1, 1, 3)
+            array(array(1, 3), 4),
+            array(array(5, 5), 10),
+            array(array(3, 9), 12),
+            array(array(4, 3), 7),
+            array(array(0, -1), -1),
         );
+    }
+
+    /**
+     * @dataProvider calcData
+     */
+    public function testAdd($data, $result)
+    {
+        $calcMock = $this->getMockBuilder('\Vit\GuestBookBundle\Commenter')
+            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock()
+        ;
+
+        $this->assertEquals($result, $calcMock->add($data[0], $data[1]));
+//        $this->assertEquals($result, ($data[0]+$data[1]));
     }
 }
